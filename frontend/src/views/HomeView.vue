056cfe48d098d2955033ece7d20f84db55317da9
invalid-code <jessgaming55@gmail.com>
2026-06-02 20:07:57 +0800

    fix: fixed search icon button not having a square aspect ratio feat: finished main dashboard page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -12,48 +12,52 @@ import imgPlaceholder from '../assets/image.png'
       <div class="px-4.75 py-5">
         <div class="flex items-center">
           <div class="w-6.25 h-6.25 bg-red-600 rounded-[50%]"></div>
-          <p class="text-[30px] whitespace-nowrap">Stock Name</p>
+          <p class="text-[30px] whitespace-nowrap ml-3.75">Stock Name</p>
         </div>
         <div class="text-[20px]">Branch Name</div>
       </div>
       <div class="px-4.75 py-5">
         <div class="flex items-center">
           <div class="w-6.25 h-6.25 bg-red-600 rounded-[50%]"></div>
-          <p class="text-[30px] whitespace-nowrap">Stock Name</p>
+          <p class="text-[30px] whitespace-nowrap ml-3.75">Stock Name</p>
         </div>
         <div class="text-[20px]">Branch Name</div>
       </div>
       <div class="px-4.75 py-5">
         <div class="flex items-center">
           <div class="w-6.25 h-6.25 bg-red-600 rounded-[50%]"></div>
-          <p class="text-[30px] whitespace-nowrap">Stock Name</p>
+          <p class="text-[30px] whitespace-nowrap ml-3.75">Stock Name</p>
         </div>
         <div class="text-[20px]">Branch Name</div>
       </div>
     </div>
     <div class="bg-white row-span-2 rounded-[25px] overflow-hidden px-7.5 py-4.5">
-      <div class="flex mb-12.75">
+      <div class="flex mb-12.75 justify-between">
         <div class="bg-[#363537] px-3.25 py-1.5 rounded-[15px] flex">
-          <input class="placeholder:text-white bg-[#5B5A5E80] rounded-[10px] pl-3 h-9.5 caret-white text-white mr-1.5"
+          <input
+            class="placeholder:text-white placeholder:text-[23px] bg-[#5B5A5E80] rounded-[10px] pl-3 caret-white text-white mr-1.5 h-full w-[320px]"
             type="text" placeholder="Search stock...">
-          <button>
+          <button class="shrink-0 bg-[#5B5A5E80] rounded-[10px] px-2 py-1">
             <img class="w-9.5 h-9.5" :src="search" alt="stock search button">
           </button>
         </div>
-        <div>
-          <select name="branches">
-            <option value="house">HOUSE</option>
-            <option value="ccmc">CCMC</option>
-            <option value="perpetual">PERPETUAL</option>
-          </select>
-        </div>
         <div class="flex">
-          <div class="w-7.5 h-7.5 bg-red-600 rounded-[50%]"></div>
-          <p>Low</p>
+          <div>
+            <select class="h-full border-[#EF2D56] text-[#EF2D56] text-[23px] font-bold border-4 text-center"
+              name="branches">
+              <option value="house">HOUSE</option>
+              <option value="ccmc">CCMC</option>
+              <option value="perpetual">PERPETUAL</option>
+            </select>
+          </div>
+          <div class="flex items-center px-3.5">
+            <div class="w-7.5 h-7.5 bg-red-600 rounded-[50%]"></div>
+            <p class="text-[23px] ml-2.75">Low</p>
+          </div>
+          <button class="bg-[#DCED31] px-3.5 py-3.75 rounded-[20px] text-[23px]">Apply</button>
         </div>
-        <button class="bg-[#DCED31] px-3.5 py-3.75 rounded-[20px]">Apply</button>
       </div>
-      <div class="grid grid-cols-[200px_200px_200px_200px] mb-6.25 gap-x-9.25 gap-y-15">
+      <div class="grid grid-cols-[200px_200px_200px_200px] mb-6.25 gap-x-9.25 gap-y-15 justify-center">
         <div class="bg-[#0CCE6B] px-3.75 py-5 rounded-[25px]">
           <img :src="imgPlaceholder" alt="placholder image">
           <div class="flex mt-2.5">
